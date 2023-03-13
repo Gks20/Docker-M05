3fbac437e2b8acf05698da9d8cbf3bd89ecc5314
Gks20 <105525491+Gks20@users.noreply.github.com>
2023-03-12 23:28:37 -0400

    Add files via upload

---
+++ Docker_Emoji/Emoji_Generator.py
@@ -0,0 +1,9 @@
+import emoji
+
+print(emoji.emojize(":grinning_face_with_big_eyes:",variant="emoji_type"))
+print(emoji.emojize(":face_with_tears_of_joy:",variant="emoji_type"))
+print(emoji.emojize(":upside-down_face:",variant="emoji_type"))
+print(emoji.emojize(":money-mouth_face:",variant="emoji_type"))
+
+
+
